fc06990a9c093914ca84da94b5bde556a12f3368
mayur-palase <38392595+mayur-palase@users.noreply.github.com>
2026-06-07 04:18:22 +0000

    custom hooks

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,9 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function useFetch(url) {
-    // const [data, ]
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
+        function fetchDataAsUrl(url) {
+            setLoading(true);
+            fetch(url)
+                .then((res) => res.json())
+                .then((data) => setData(data))
+                .catch((error) => setError(error || "Something went wrong..."))
+                .finally(() => setLoading(false))
+        }
 
+        fetchDataAsUrl(url);
     }, [url])
+
+    if(loading) <div>Loading...</div>
+
+    if(error) return error;
+
+    return {data, loading, error}
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import withFetch from '../HOC/withFetch'
+import useFetch from '../Hooks/useFetch';
 
 function Posts({data, error, loading}) {
 
+    // This is example of HOC
     // const [data, error, loading] = withFetch('https://jsonplaceholder.typicode.com/posts')
+
+    // This is example of custom hook
+    const {data: postsData} = useFetch('https://jsonplaceholder.typicode.com/posts?_page=1&_limit=10');
+
     console.log(data);
 
   return (
     <>
       <ul>
         {
-          data?.map((item) => {
-            return <li>{item.title}</li>
+          postsData?.map((item) => {
+            return <li key={item.id}>{item.title}</li>
           })
         }
       </ul>
